ab6c1e86f23dda502b21abe66dbcfda20cde8133
Ranidu Gurusinghe <ranidugurusinghe@Ranidus-MacBook-Air.local>
2025-06-29 14:14:02 +0530

    moved testimonial section up in homme page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import WelcomeBanner from '../Components/WelcomeBanner/WelcomeBanner';
 import FeaturesSection from '../Components/FeaturesSection/FeaturesSection';
-import ProductShowcase from '../Components/ProductShowcase/ProductShowcase';
 import CustomizationProcess from '../Components/CustomizationProcess/CustomizationProcess';
 import TestimonialSlider from '../Components/TestimonialSlider/TestimonialSlider';
 import CollectionsShowcase from '../Components/CollectionsShowcase/CollectionsShowcase';
@@ -10,10 +9,10 @@ const Home = () => {
   return (
     <div className="home-page">
       <WelcomeBanner />
+      <TestimonialSlider />
       <FeaturesSection />
       <CollectionsShowcase />
       <CustomizationProcess />
-      <TestimonialSlider />
     </div>
   );
 };
